484cc8ae14947096c73b22fa2b42638cca43111a
TobyKreiselmaier <63912793+TobyKreiselmaier@users.noreply.github.com>
2020-10-13 20:30:11 -0400

    Fixed errors

--- main.js
+++ main.js
@@ -3,14 +3,13 @@ var instance;
 
 $(document).ready(function() {
     window.ethereum.enable().then(function(accounts){ //access to Metamask
-      instance = new web3.eth.Contract(window.abi, "0x84f5d9Efd7c53C029DC6680F1995b5Cb8b19E82a", {from: accounts[0]});  //allows you to interact with smart contracts as if they were JavaScript objects.
-      console.log(instance);
+      instance = new web3.eth.Contract(abi, "0x84f5d9Efd7c53C029DC6680F1995b5Cb8b19E82a", {from: accounts[0]});  //allows you to interact with smart contracts as if they were JavaScript objects.
     });
-    $("#add_data_button").click(inputData)
-    $("#get_data_button").click(fetchAndDisplayData)
+    $("#add_data_button").click(inputData);
+    $("#get_data_button").click(fetchAndDisplayData);
 });
 
-function inputData(){
+async function inputData(){
   var name = $("#name_input").val();
   var age = $("#age_input").val();
   var height = $("#height_input").val();
@@ -19,7 +18,7 @@ function inputData(){
     value: web3.utils.toWei("1", "ether")
   };
 
-  instance.methods.createPerson(name, age, height).send(config)
+  await instance.methods.createPerson(name, age, height).send(config)
   .on("transactionHash", function(hash){
     console.log(hash);
   })//event listener web3
@@ -32,8 +31,8 @@ function inputData(){
   })
 }
 
-function fetchAndDisplayData(){
-    instance.methods.getPerson().call().then(function(res){
+async function fetchAndDisplayData(){
+    await instance.methods.getPerson().call().then(function(res){
       $("#name_output").text(res.name);
       $("#age_output").text(res.age);
       $("#height_output").text(res.height);
